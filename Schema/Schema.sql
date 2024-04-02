1148314a54793bc3849c11d167f7bb07c1d04ed3
Dave550 <82191184+Dave550@users.noreply.github.com>
2024-04-02 19:22:38 -0400

    new tables

--- Schema/Schema.sql
+++ Schema/Schema.sql
@@ -1,15 +1,5 @@
 CREATE SCHEMA IF NOT EXISTS TheBestDBEver;
-
-CREATE TABLE IF NOT EXISTS thebestdbever.shift
-(
-    SSN char(9) NOT NULL,
-    Date date NOT NULL,
-    StartTime time NOT NULL,
-    Duration interval NOT NULL,
-
-    PRIMARY KEY (SSN)
-);
-
+SET SCHEMA 'thebestdbever';
 CREATE TABLE IF NOT EXISTS thebestdbever.shift
 (
     ShiftID serial NOT NULL,
@@ -18,4 +8,33 @@ CREATE TABLE IF NOT EXISTS thebestdbever.shift
     Duration interval NOT NULL,
 
     PRIMARY KEY (ShiftID)
+);
+
+CREATE TYPE position AS ENUM (
+    'Roast Chef',
+    'Sous Chef',
+    'Pastry Chef',
+    'Executive Chef',
+    'Hosts',
+    'Manager',
+    'Sommelier',
+    'Runner',
+    'Captain',
+    'Waiter',
+    'Busboy',
+    'Back Waiter',
+    'Bartender',
+    'Barback'
+    );
+
+CREATE TABLE IF NOT EXISTS thebestdbever.employee
+(
+    SSN char(9) NOT NULL,
+    FirstName varchar(255) NOT NULL,
+    LastName varchar(255) NOT NULL,
+    StartDate date NOT NULL,
+    PhoneNumber varchar(11) NOT NULL,
+    Pos thebestdbever.position NOT NULL,
+
+    PRIMARY KEY (SSN)
 );
