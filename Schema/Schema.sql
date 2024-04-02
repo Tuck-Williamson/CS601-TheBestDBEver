2c5b814ce19fef5f847fbdebd03a5e5d3a2a0cca
Dave550 <82191184+Dave550@users.noreply.github.com>
2024-04-02 19:04:38 -0400

    Adding shift

---
+++ Schema/Schema.sql
@@ -0,0 +1,11 @@
+CREATE SCHEMA IF NOT EXISTS TheBestDBEver;
+
+CREATE TABLE IF NOT EXISTS thebestdbever.shift
+(
+    SSN char(9) NOT NULL,
+    Date date NOT NULL,
+    StartTime time NOT NULL,
+    Duration interval NOT NULL,
+
+    PRIMARY KEY (SSN)
+);
