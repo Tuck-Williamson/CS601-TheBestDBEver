4d2f7fda54405acaccaee1d4fa0ca2ac223f9e97
Dave550 <82191184+Dave550@users.noreply.github.com>
2024-04-09 18:29:36 -0400

    added top and reservation, also changed shiftid to a int

--- Schema/Schema.sql
+++ Schema/Schema.sql
@@ -46,28 +46,62 @@ CREATE TABLE IF NOT EXISTS thebestdbever.employee
 DROP TABLE IF EXISTS thebestdbever.works_on_shift;
 CREATE TABLE works_on_shift
 (
-    shiftid        SERIAL        NOT NULL
+    shiftid        int        NOT NULL
         CONSTRAINT wos_shift___fk
             REFERENCES shift
-            ON UPDATE RESTRICT ON DELETE RESTRICT,
+            ON UPDATE CASCADE ON DELETE CASCADE,
     ssn            varchar(9) NOT NULL
-        CONSTRAINT "wos_SSN___fk"
+        CONSTRAINT wos_SSN___fk
             REFERENCES employee
-            ON UPDATE RESTRICT ON DELETE RESTRICT,
-    "HeadPosition" thebestdbever.position,
-    CONSTRAINT works_on_shift_pk
-        PRIMARY KEY (shiftid, ssn)
+            ON UPDATE CASCADE ON DELETE CASCADE,
+    HeadPosition thebestdbever.position,
+    PRIMARY KEY (shiftid, ssn)
 );
 
 DROP TABLE IF EXISTS thebestdbever.customer;
 CREATE TABLE customer
 (
     CustomerID SERIAL NOT NULL,
-    LastName varchar(255) NOT NULL ,
-    FirstName varchar(255),
-    PhoneNumber varchar(11) NOT NULL,
-    Comment varchar(255),
-    VIP bool,
+    LastName VARCHAR(255) NOT NULL ,
+    FirstName VARCHAR(255),
+    PhoneNumber VARCHAR(11) NOT NULL,
+    Comment VARCHAR(255),
+    VIP BOOL,
 
     PRIMARY KEY (CustomerID)
+);
+
+DROP TABLE IF EXISTS thebestdbever.top;
+CREATE TABLE top
+(
+    TopID SERIAL NOT NULL,
+    Capacity INT NOT NULL,
+    Type VARCHAR(255) NOT NULL,
+
+    PRIMARY KEY(TopID)
+
+);
+
+DROP TABLE IF EXISTS thebestdbever.reservation;
+CREATE TABLE reservation
+(
+    ReservationID serial NOT NULL,
+    CustomerID int NOT NULL
+        CONSTRAINT CID___fk
+        REFERENCES thebestdbever.customer
+        ON UPDATE CASCADE ON DELETE CASCADE,
+
+    Date date NOT NULL,
+    Time time NOT NULL,
+    PartySize INT NOT NULL,
+    Occasion varchar(255),
+    Comment varchar(255),
+    SeatedTime time NOT NULL,
+    TopID INT NOT NULL
+    CONSTRAINT TID___fk
+    REFERENCES thebestdbever.top
+    ON UPDATE CASCADE ON DELETE CASCADE,
+
+    PRIMARY KEY(ReservationID)
+
 );
