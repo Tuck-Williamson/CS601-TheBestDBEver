6471f5c9cea8b030ab8360fbeda177d34e29c923
Tuck Williamson <tuckwilliamson@gmail.com>
2024-04-11 15:40:46 -0400

    Almost done. Just need to add query for night shift ->  needs Sommelier = true.

--- Schema/Schema.sql
+++ Schema/Schema.sql
@@ -1,13 +1,13 @@
 CREATE SCHEMA IF NOT EXISTS TheBestDBEver;
 SET SCHEMA 'thebestdbever';
 
-DROP TABLE IF EXISTS thebestdbever.shift CASCADE ;
+DROP TABLE IF EXISTS thebestdbever.shift CASCADE;
 CREATE TABLE IF NOT EXISTS thebestdbever.shift
 (
-    ShiftID serial NOT NULL,
-    Date date NOT NULL,
-    StartTime time NOT NULL,
-    Duration interval NOT NULL,
+    ShiftID   serial   NOT NULL,
+    Date      date     NOT NULL,
+    StartTime time     NOT NULL,
+    Duration  interval NOT NULL,
 
     PRIMARY KEY (ShiftID)
 );
@@ -34,12 +34,12 @@ CREATE TYPE position AS ENUM (
 DROP TABLE IF EXISTS thebestdbever.employee CASCADE;
 CREATE TABLE IF NOT EXISTS thebestdbever.employee
 (
-    SSN char(9) NOT NULL,
-    FirstName varchar(255) NOT NULL,
-    LastName varchar(255) NOT NULL,
-    StartDate date NOT NULL,
-    PhoneNumber varchar(11) NOT NULL,
-    Pos thebestdbever.position NOT NULL,
+    SSN         char(9)                NOT NULL,
+    FirstName   varchar(255)           NOT NULL,
+    LastName    varchar(255)           NOT NULL,
+    StartDate   date                   NOT NULL,
+    PhoneNumber varchar(11)            NOT NULL,
+    Pos         thebestdbever.position NOT NULL,
 
     PRIMARY KEY (SSN)
 );
@@ -47,11 +47,11 @@ CREATE TABLE IF NOT EXISTS thebestdbever.employee
 DROP TABLE IF EXISTS thebestdbever.works_on_shift;
 CREATE TABLE works_on_shift
 (
-    shiftid        int        NOT NULL
+    shiftid      int        NOT NULL
         CONSTRAINT wos_shift___fk
             REFERENCES shift
             ON UPDATE CASCADE ON DELETE CASCADE,
-    ssn            varchar(9) NOT NULL
+    ssn          varchar(9) NOT NULL
         CONSTRAINT wos_SSN___fk
             REFERENCES employee
             ON UPDATE CASCADE ON DELETE CASCADE,
@@ -59,126 +59,16 @@ CREATE TABLE works_on_shift
     PRIMARY KEY (shiftid, ssn)
 );
 
-DROP FUNCTION IF EXISTS wos_get_filled_positions;
-CREATE OR REPLACE FUNCTION wos_get_filled_positions(sId int) RETURNS TABLE (p thebestdbever.position)
-AS
-$$
-BEGIN
-    RETURN QUERY (SELECT works_on_shift.HeadPosition FROM works_on_shift WHERE shiftid = sId);
-END;
-$$ LANGUAGE plpgsql;
-DROP FUNCTION IF EXISTS wos_position_filled;
-CREATE OR REPLACE FUNCTION wos_get_filled_positions(sId int) RETURNS TABLE (p thebestdbever.position)
-AS
-$$
-BEGIN
-    RETURN QUERY (SELECT works_on_shift.HeadPosition FROM works_on_shift WHERE shiftid = sId);
-END;
-$$ LANGUAGE plpgsql;
-
-SELECT wos_get_filled_positions(CAST(1 AS INT));
-
-CREATE OR REPLACE FUNCTION wos_trigger_check_all_pos_filled()
-    RETURNS TRIGGER AS
-$wos_trigger_check_all_pos_filled$
-DECLARE
-    new_pos    thebestdbever.position;
-    filled_pos  thebestdbever.position[];
---     none_filled bool;
-    -- These are position subtype arrays.
-    -- The canonical HeadPosition for each subtype is the first value in the array.
-    chefs      thebestdbever.Position[] = ARRAY ['Roast Chef', 'Sous Chef', 'Pastry Chef', 'Executive Chef'];
-    waiters    thebestdbever.Position[] = ARRAY ['Sommelier', 'Runner', 'Captain', 'Waiter', 'Busboy', 'Back Waiter'];
-    bartenders thebestdbever.Position[] = ARRAY ['Bartender', 'Barback'];
-BEGIN
-    RAISE NOTICE 'Begin: Works On Shift Trigger- check all positions filled.';
-    IF NEW.HeadPosition IS NULL THEN
-        -- Store the new data's position, then load all filled positions.
-        SELECT thebestdbever.employee.pos INTO new_pos FROM thebestdbever.employee WHERE ssn = NEW.ssn LIMIT 1;
-        filled_pos := ARRAY [(SELECT works_on_shift.HeadPosition FROM works_on_shift WHERE shiftid = NEW.shiftid)];
---         none_filled := cardinality(filled_pos) = 0;
-        RAISE WARNING ':: new position: %, filled: %', new_pos, filled_pos;
-        RAISE WARNING 'WHEN % = ANY (%) AND NOT % = ANY (%) THEN', new_pos, chefs, chefs[1], filled_pos;
-
-        -- Check to see if this position already has a head. If not then make this employee the head.
-        CASE new_pos
-            WHEN new_pos = ANY (chefs) AND NOT chefs[1] = ANY (filled_pos) THEN
-                -- Chefs doesn't have a head - set the new row's HeadPosition to the canonical value for chefs.
-                -- The canonical value is why we can just check chefs[0] NOT IN (filled_pos) instead of each
-                -- item in filled_pos against chefs.
-                NEW.HeadPosition := chefs[1];
-            WHEN new_pos = ANY (waiters) AND NOT waiters[1] = ANY (filled_pos) THEN
-                NEW.HeadPosition := waiters[1];
-            WHEN new_pos = ANY (bartenders) AND NOT bartenders[1] = ANY (filled_pos) THEN
-                NEW.HeadPosition := bartenders[1];
-            WHEN NOT new_pos = ANY (filled_pos)
-                THEN -- We can just check against filled_pos because the rest of the positions don't have sub-specialties
-                NEW.HeadPosition := new_pos;
-        END CASE;
-    END IF;
-
-    RAISE NOTICE 'End: Works On Shift Trigger - check all positions filled.';
-    RETURN NEW;
-END ;
-$wos_trigger_check_all_pos_filled$ LANGUAGE plpgsql;
-
--- Plan B
-CREATE OR REPLACE FUNCTION wos_trigger_check_all_pos_filled()
-    RETURNS TRIGGER AS
-$wos_trigger_check_all_pos_filled$
-DECLARE
-    new_pos    varchar;
-    filled_pos  thebestdbever.position[];
-    -- These are position subtype arrays.
-    -- The canonical HeadPosition for each subtype is the first value in the array.
-    chefs      thebestdbever.Position[] = ARRAY ['Roast Chef', 'Sous Chef', 'Pastry Chef', 'Executive Chef'];
-    waiters    thebestdbever.Position[] = ARRAY ['Sommelier', 'Runner', 'Captain', 'Waiter', 'Busboy', 'Back Waiter'];
-    bartenders thebestdbever.Position[] = ARRAY ['Bartender', 'Barback'];
-BEGIN
-    RAISE NOTICE 'Begin: Works On Shift Trigger- check all positions filled.';
-    IF NEW.HeadPosition IS NULL THEN
-        -- Store the new data's position, then load all filled positions.
-        SELECT thebestdbever.employee.pos INTO new_pos FROM thebestdbever.employee WHERE ssn = NEW.ssn LIMIT 1;
-        filled_pos := ARRAY [(SELECT works_on_shift.HeadPosition FROM works_on_shift WHERE shiftid = NEW.shiftid)];
-        RAISE WARNING ':: new position: %, filled: %', new_pos, filled_pos;
-        RAISE WARNING 'WHEN % = ANY (%) AND NOT % = ANY (%) THEN', new_pos, chefs, chefs[1], filled_pos;
-
-        -- Check to see if this position already has a head. If not then make this employee the head.
-        CASE new_pos
-            WHEN new_pos = ANY (chefs) AND NOT chefs[1] = ANY (filled_pos) THEN
-                -- Chefs doesn't have a head - set the new row's HeadPosition to the canonical value for chefs.
-                -- The canonical value is why we can just check chefs[0] NOT IN (filled_pos) instead of each
-                -- item in filled_pos against chefs.
-                NEW.HeadPosition := chefs[1];
-            WHEN new_pos = ANY (waiters) AND NOT waiters[1] = ANY (filled_pos) THEN
-                NEW.HeadPosition := waiters[1];
-            WHEN new_pos = ANY (bartenders) AND NOT bartenders[1] = ANY (filled_pos) THEN
-                NEW.HeadPosition := bartenders[1];
-            WHEN NOT new_pos = ANY (filled_pos)
-                THEN -- We can just check against filled_pos because the rest of the positions don't have sub-specialties
-                NEW.HeadPosition := new_pos;
-        END CASE;
-    END IF;
-
-    RAISE NOTICE 'End: Works On Shift Trigger - check all positions filled.';
-    RETURN NEW;
-END ;
-$wos_trigger_check_all_pos_filled$ LANGUAGE plpgsql;
-
-CREATE OR REPLACE TRIGGER wos_trigger_check_wos
-    BEFORE INSERT
-    ON works_on_shift
-    FOR EACH ROW EXECUTE FUNCTION wos_trigger_check_all_pos_filled();
 
 DROP TABLE IF EXISTS thebestdbever.customer;
 CREATE TABLE customer
 (
-    CustomerID SERIAL NOT NULL,
-    LastName VARCHAR(255) NOT NULL ,
-    FirstName VARCHAR(255),
-    PhoneNumber VARCHAR(11) NOT NULL,
-    Comment VARCHAR(255),
-    VIP BOOL,
+    CustomerID  SERIAL       NOT NULL,
+    LastName    VARCHAR(255) NOT NULL,
+    FirstName   VARCHAR(255),
+    PhoneNumber VARCHAR(11)  NOT NULL,
+    Comment     VARCHAR(255),
+    VIP         BOOL,
 
     PRIMARY KEY (CustomerID)
 );
@@ -186,11 +76,11 @@ CREATE TABLE customer
 DROP TABLE IF EXISTS thebestdbever.top;
 CREATE TABLE top
 (
-    TopID SERIAL NOT NULL,
-    Capacity INT NOT NULL,
-    Type VARCHAR(255) NOT NULL,
+    TopID    SERIAL       NOT NULL,
+    Capacity INT          NOT NULL,
+    Type     VARCHAR(255) NOT NULL,
 
-    PRIMARY KEY(TopID)
+    PRIMARY KEY (TopID)
 
 );
 
@@ -198,22 +88,155 @@ DROP TABLE IF EXISTS thebestdbever.reservation;
 CREATE TABLE reservation
 (
     ReservationID serial NOT NULL,
-    CustomerID int NOT NULL
+    CustomerID    int    NOT NULL
         CONSTRAINT CID___fk
-        REFERENCES thebestdbever.customer
-        ON UPDATE CASCADE ON DELETE CASCADE,
+            REFERENCES thebestdbever.customer
+            ON UPDATE CASCADE ON DELETE CASCADE,
 
-    Date date NOT NULL,
-    Time time NOT NULL,
-    PartySize INT NOT NULL,
-    Occasion varchar(255),
-    Comment varchar(255),
-    SeatedTime time NOT NULL,
-    TopID INT NOT NULL
-    CONSTRAINT TID___fk
-    REFERENCES thebestdbever.top
-    ON UPDATE CASCADE ON DELETE CASCADE,
+    Date          date   NOT NULL,
+    Time          time   NOT NULL,
+    PartySize     INT    NOT NULL,
+    Occasion      varchar(255),
+    Comment       varchar(255),
+    SeatedTime    time   NOT NULL,
+    TopID         INT    NOT NULL
+        CONSTRAINT TID___fk
+            REFERENCES thebestdbever.top
+            ON UPDATE CASCADE ON DELETE CASCADE,
 
-    PRIMARY KEY(ReservationID)
+    PRIMARY KEY (ReservationID)
 
 );
+
+
+DROP FUNCTION IF EXISTS wos_canon_pos;
+-- This function gets the 'canonical' position from the given employee's position.
+-- Making a single element of sub-specialities 'canon' just makes the checking
+--  for HeadPositions easier.
+-- Param: sommelierRequired - should be True for evening shifts.
+CREATE OR REPLACE FUNCTION wos_canon_pos(empPos thebestdbever.position,
+                                         sommelierRequired bool)
+    RETURNS thebestdbever.position
+AS
+$$
+DECLARE
+    -- These are position subtype arrays.
+    -- The canonical HeadPosition for each subtype is the first value in
+    --  the array. Because of this Sommelier cannot be the first one in
+    --  the waiter category.
+    chefs          thebestdbever.Position[] = ARRAY ['Roast Chef', 'Sous Chef',
+        'Pastry Chef', 'Executive Chef'];
+    waiters        thebestdbever.Position[] = ARRAY ['Captain', 'Sommelier', 'Runner',
+        'Waiter', 'Busboy', 'Back Waiter'];
+    waitersNoSomm  thebestdbever.Position[] = ARRAY ['Captain', 'Runner',
+        'Waiter', 'Busboy', 'Back Waiter'];
+    bartenders     thebestdbever.Position[] = ARRAY ['Bartender', 'Barback'];
+    waitersToCheck thebestdbever.position[] := waiters;
+BEGIN
+    -- This is how we handle Sommelier differently
+    --  if sommelierRequired (it's a night shift).
+    IF sommelierRequired THEN
+        waitersToCheck := waitersNoSomm;
+    end if;
+    IF empPos = ANY (chefs) THEN
+        RETURN chefs[1];
+    ELSEIF empPos = ANY (waitersToCheck) THEN
+        RETURN waiters[1];
+    ELSEIF empPos = ANY (bartenders) THEN
+        RETURN bartenders[1];
+    ELSE
+        -- We can just check against filled_pos because the
+        --  rest of the positions don't have sub-specialties.
+        -- This is the fall thru for Sommelier if sommelierRequired.
+        RETURN empPos;
+    END IF;
+END;
+$$ LANGUAGE plpgsql;
+
+-- From our requirements document this addresses business requirement in 2.I, 2.II
+CREATE OR REPLACE FUNCTION wos_trigger_check_pos_filled()
+    RETURNS TRIGGER AS
+$wos_trigger_check_all_pos_filled$
+DECLARE
+    new_pos           thebestdbever.position;
+    sommelierRequired bool;
+BEGIN
+--     RAISE NOTICE 'Begin %: Works On Shift Trigger- check all positions filled.', tg_name;
+    IF NEW.HeadPosition IS NULL THEN
+        -- We get the position from the employee ssn; however, for sub-specialties
+        --  we make one of them 'cannon' that stand for all the sub-specialties in
+        --  each parent specialty.
+        new_pos :=
+                wos_canon_pos(cast((SELECT e.pos
+                                    FROM thebestdbever.employee as e
+                                    WHERE e.ssn = NEW.ssn
+                                    LIMIT 1) as thebestdbever.position),
+                              FALSE);
+
+        -- We check if the position already is filled.
+        --  If so we don't need to make this one the HeadPosition otherwise just set HeadPosition
+        IF new_pos NOT IN
+           (SELECT DISTINCT works_on_shift.HeadPosition FROM works_on_shift WHERE shiftid = NEW.shiftid) THEN
+            RAISE NOTICE '::% - Setting employee as HeadPosition for shift. Data: %', tg_name, NEW;
+            NEW.HeadPosition = new_pos;
+        END IF;
+    END IF;
+
+--     RAISE NOTICE 'End %: Works On Shift Trigger - check all positions filled.', tg_name;
+    RETURN NEW;
+END ;
+$wos_trigger_check_all_pos_filled$ LANGUAGE plpgsql;
+
+
+CREATE OR REPLACE TRIGGER wos_trigger_check_wos_insert
+    BEFORE INSERT
+    ON works_on_shift
+    FOR EACH ROW
+EXECUTE FUNCTION wos_trigger_check_pos_filled();
+
+------------------------------------------------------
+-- Keep these at the bottom so any errors
+--  are printed last in the console.
+------------------------------------------------------
+-- Code checks
+
+do
+$$
+    DECLARE
+        --Get the canon waiter, bartender, and chef
+        waitCanon thebestdbever.position;
+        barCanon  thebestdbever.position;
+        chefCanon thebestdbever.position;
+    BEGIN
+        RAISE NOTICE 'Beginning Test Cases.';
+
+        waitCanon := wos_canon_pos(empPos := 'Back Waiter', sommelierRequired := false);
+        IF wos_canon_pos(empPos := 'Busboy', sommelierRequired := FALSE) <> waitCanon OR
+           wos_canon_pos(empPos := 'Captain', sommelierRequired := FALSE) <> waitCanon OR
+           wos_canon_pos(empPos := 'Runner', sommelierRequired := FALSE) <> waitCanon OR
+           wos_canon_pos(empPos := 'Sommelier', sommelierRequired := FALSE) <> waitCanon OR
+           wos_canon_pos(empPos := 'Waiter', sommelierRequired := FALSE) <> waitCanon THEN
+            RAISE EXCEPTION 'Waiter mapping incorrect.';
+        ELSE
+            RAISE NOTICE '::Waiters passed.';
+        END IF;
+
+        barCanon := wos_canon_pos(empPos := 'Barback', sommelierRequired := false);
+        IF wos_canon_pos(empPos := 'Bartender', sommelierRequired := FALSE) <> barCanon THEN
+            RAISE EXCEPTION 'Bartender mapping incorrect.';
+        ELSE
+            RAISE NOTICE '::Bartenders passed.';
+        END IF;
+
+        chefCanon := wos_canon_pos(empPos := 'Sous Chef', sommelierRequired := false);
+        IF wos_canon_pos(empPos := 'Roast Chef', sommelierRequired := FALSE) <> chefCanon OR
+           wos_canon_pos(empPos := 'Pastry Chef', sommelierRequired := FALSE) <> chefCanon OR
+           wos_canon_pos(empPos := 'Executive Chef', sommelierRequired := FALSE) <> chefCanon THEN
+            RAISE EXCEPTION 'Chef mapping incorrect.';
+        ELSE
+            RAISE NOTICE '::Chefs passed.';
+        END IF;
+
+        RAISE NOTICE 'Tests complete.';
+    END;
+$$ LANGUAGE plpgsql;
